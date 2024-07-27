f3b4114cf22dee94e34946500190bda2649edfa2
Sasha Fedchin <sasha.fedchin@gmail.com>
2024-07-27 08:17:35 +0200

    Add commons-cli library

--- data/evaluate_on_acrostics-identification-task-dataset.sh
+++ data/evaluate_on_acrostics-identification-task-dataset.sh
@@ -3,24 +3,24 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path" || exit
 
 cd ../python
-# python3 tokenizer.py ../../acrostics-identification-task-dataset/en EN100 100 EN utf-8
-# python3 tokenizer.py ../../acrostics-identification-task-dataset/en EN300 300 EN utf-8
-# python3 tokenizer.py ../../acrostics-identification-task-dataset/en EN900 900 EN utf-8
-# python3 tokenizer.py ../../acrostics-identification-task-dataset/en EN2700 2700 EN utf-8
-# python3 tokenizer.py ../../acrostics-identification-task-dataset/en EN8100 8100 EN utf-8
-# python3 tokenizer.py ../../acrostics-identification-task-dataset/en EN24300 24300 EN utf-8
-# python3 tokenizer.py ../../acrostics-identification-task-dataset/en EN72900 72900 EN utf-8
-# python3 tokenizer.py ../../acrostics-identification-task-dataset/ru RU100 100 RU utf-8
-python3 tokenizer.py ../../acrostics-identification-task-dataset/ru RU300 300 RU utf-8
-python3 tokenizer.py ../../acrostics-identification-task-dataset/ru RU900 900 RU utf-8
-python3 tokenizer.py ../../acrostics-identification-task-dataset/ru RU2700 2700 RU utf-8
-python3 tokenizer.py ../../acrostics-identification-task-dataset/ru RU8100 8100 RU utf-8
-python3 tokenizer.py ../../acrostics-identification-task-dataset/ru RU24300 24300 RU utf-8
-python3 tokenizer.py ../../acrostics-identification-task-dataset/ru RU72900 72900 RU utf-8
-python3 tokenizer.py ../../acrostics-identification-task-dataset/fr FR100 100 FR utf-8
-python3 tokenizer.py ../../acrostics-identification-task-dataset/fr FR300 300 FR utf-8
-python3 tokenizer.py ../../acrostics-identification-task-dataset/fr FR900 900 FR utf-8
-python3 tokenizer.py ../../acrostics-identification-task-dataset/fr FR2700 2700 FR utf-8
-python3 tokenizer.py ../../acrostics-identification-task-dataset/fr FR8100 8100 FR utf-8
-python3 tokenizer.py ../../acrostics-identification-task-dataset/fr FR24300 24300 FR utf-8
-python3 tokenizer.py ../../acrostics-identification-task-dataset/fr FR72900 72900 FR utf-8
+# python3 tokenizer.py ../../acrostic-identification-task-dataset/en EN100 100 EN utf-8
+# python3 tokenizer.py ../../acrostic-identification-task-dataset/en EN300 300 EN utf-8
+# python3 tokenizer.py ../../acrostic-identification-task-dataset/en EN900 900 EN utf-8
+# python3 tokenizer.py ../../acrostic-identification-task-dataset/en EN2700 2700 EN utf-8
+# python3 tokenizer.py ../../acrostic-identification-task-dataset/en EN8100 8100 EN utf-8
+# python3 tokenizer.py ../../acrostic-identification-task-dataset/en EN24300 24300 EN utf-8
+# python3 tokenizer.py ../../acrostic-identification-task-dataset/en EN72900 72900 EN utf-8
+# python3 tokenizer.py ../../acrostic-identification-task-dataset/ru RU100 100 RU utf-8
+# python3 tokenizer.py ../../acrostic-identification-task-dataset/ru RU300 300 RU utf-8
+python3 tokenizer.py ../data/acrostic-identification-task-dataset/ru RU900 900 RU utf-8
+python3 tokenizer.py ../data/acrostic-identification-task-dataset/ru RU2700 2700 RU utf-8
+python3 tokenizer.py ../data/acrostic-identification-task-dataset/ru RU8100 8100 RU utf-8
+python3 tokenizer.py ../data/acrostic-identification-task-dataset/ru RU24300 24300 RU utf-8
+python3 tokenizer.py ../data/acrostic-identification-task-dataset/ru RU72900 72900 RU utf-8
+python3 tokenizer.py ../data/acrostic-identification-task-dataset/fr FR100 100 FR utf-8
+python3 tokenizer.py ../data/acrostic-identification-task-dataset/fr FR300 300 FR utf-8
+python3 tokenizer.py ../data/acrostic-identification-task-dataset/fr FR900 900 FR utf-8
+python3 tokenizer.py ../data/acrostic-identification-task-dataset/fr FR2700 2700 FR utf-8
+python3 tokenizer.py ../data/acrostic-identification-task-dataset/fr FR8100 8100 FR utf-8
+python3 tokenizer.py ../data/acrostic-identification-task-dataset/fr FR24300 24300 FR utf-8
+python3 tokenizer.py ../data/acrostic-identification-task-dataset/fr FR72900 72900 FR utf-8
