2e31a9ffbd6e0c53c17a7f34f176e8f46ab8bab9
Sasha Fedchin <sasha.fedchin@gmail.com>
2024-07-29 12:36:32 +0200

    Debugging

--- src/acrostics/LanguageModel.java
+++ src/acrostics/LanguageModel.java
@@ -18,18 +18,13 @@ public class LanguageModel {
 
     public LanguageModel(String filename) {
         filename = filename + FILE_POSTFIX;
-        System.err.println("In constructor");
         try {
             Scanner sc = new Scanner(Paths.get(filename), "UTF-8");
-            System.err.println("Reading file: " + filename);
-            System.err.println("Reading file: " + new File(filename).getAbsolutePath());
             while (sc.hasNextLine()) {
                 String[] line = sc.nextLine().split("\t");
                 if ((line.length == 0) || Arrays.stream(TOKENS_TO_IGNORE).anyMatch(token -> token.equals(line[0]))) {
-                    System.err.println("Skipping line");
                     continue;
                 }
-                System.err.println("Adding a token");
                 tokenFrequencies.put(line[0], Math.pow(Math.E, Double.parseDouble(line[1])));  // TODO: Use logs
             }
         } catch (Exception e) {
